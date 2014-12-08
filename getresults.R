97a625aef2fb16f4d19887ef0f663af29f6bfa11
Anna Laurinavichyute <annlaurin@gmail.com>
2014-12-08 12:29:41 +0300

    changing audio 09

--- getresults.R
+++ getresults.R
@@ -1,5 +1,5 @@
 # where your experiment is hosted
-experigen.sourceURL = "hse-ru.github.io.nl."			### CHNGE!
+experigen.sourceURL = "annlaurin.github.io.list4"
 # this information comes from your settings.js file
 experigen.experimentName = "HSElist4"
 experigen.database = "http://db.phonologist.org/"
